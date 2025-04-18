5f6bfad365f86bf9eecfc3723a6bf5282edcac96
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2025-04-18 16:37:14 -0400

    update
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/dashboards.tf
+++ terraform/dashboards.tf
@@ -1,191 +1,155 @@
 resource "null_resource" "upload_dashboard_cpu_metrics_dashboard" {
   provisioner "local-exec" {
-    command = "${path.module}/scripts/upload_dashboard.sh ${path.module}/dashboards/cpu_metrics_dashboard.json ${grafana_folder.CPU.uid} ${var.grafana_instance_url} ${var.grafana_service_account_token}"
+    command = "${path.module}/scripts/upload_dashboard.sh ${path.module}/dashboards/cpu_metrics_dashboard.json ${grafana_folder.cpu.uid} ${var.grafana_instance_url} ${var.grafana_service_account_token}"
   }
-
   provisioner "local-exec" {
     command = "cat /tmp/grafana_upload_cpu_metrics_dashboard.log || echo '⚠️ Log not found'"
   }
-
   triggers = {
     always_run = timestamp()
   }
-
-  depends_on = [grafana_folder.CPU]
+  depends_on = [grafana_folder.cpu]
 }
 
 resource "null_resource" "upload_dashboard_gpu_metrics_dashboard" {
   provisioner "local-exec" {
-    command = "${path.module}/scripts/upload_dashboard.sh ${path.module}/dashboards/gpu_metrics_dashboard.json ${grafana_folder.GPU.uid} ${var.grafana_instance_url} ${var.grafana_service_account_token}"
+    command = "${path.module}/scripts/upload_dashboard.sh ${path.module}/dashboards/gpu_metrics_dashboard.json ${grafana_folder.gpu.uid} ${var.grafana_instance_url} ${var.grafana_service_account_token}"
   }
-
   provisioner "local-exec" {
     command = "cat /tmp/grafana_upload_gpu_metrics_dashboard.log || echo '⚠️ Log not found'"
   }
-
   triggers = {
     always_run = timestamp()
   }
-
-  depends_on = [grafana_folder.GPU]
+  depends_on = [grafana_folder.gpu]
 }
 
 resource "null_resource" "upload_dashboard_memory_metrics_dashboard" {
   provisioner "local-exec" {
-    command = "${path.module}/scripts/upload_dashboard.sh ${path.module}/dashboards/memory_metrics_dashboard.json ${grafana_folder.Memory.uid} ${var.grafana_instance_url} ${var.grafana_service_account_token}"
+    command = "${path.module}/scripts/upload_dashboard.sh ${path.module}/dashboards/memory_metrics_dashboard.json ${grafana_folder.memory.uid} ${var.grafana_instance_url} ${var.grafana_service_account_token}"
   }
-
   provisioner "local-exec" {
     command = "cat /tmp/grafana_upload_memory_metrics_dashboard.log || echo '⚠️ Log not found'"
   }
-
   triggers = {
     always_run = timestamp()
   }
-
-  depends_on = [grafana_folder.Memory]
+  depends_on = [grafana_folder.memory]
 }
 
 resource "null_resource" "upload_dashboard_disk_metrics_dashboard" {
   provisioner "local-exec" {
-    command = "${path.module}/scripts/upload_dashboard.sh ${path.module}/dashboards/disk_metrics_dashboard.json ${grafana_folder.Disk.uid} ${var.grafana_instance_url} ${var.grafana_service_account_token}"
+    command = "${path.module}/scripts/upload_dashboard.sh ${path.module}/dashboards/disk_metrics_dashboard.json ${grafana_folder.disk.uid} ${var.grafana_instance_url} ${var.grafana_service_account_token}"
   }
-
   provisioner "local-exec" {
     command = "cat /tmp/grafana_upload_disk_metrics_dashboard.log || echo '⚠️ Log not found'"
   }
-
   triggers = {
     always_run = timestamp()
   }
-
-  depends_on = [grafana_folder.Disk]
+  depends_on = [grafana_folder.disk]
 }
 
 resource "null_resource" "upload_dashboard_network_metrics_dashboard" {
   provisioner "local-exec" {
-    command = "${path.module}/scripts/upload_dashboard.sh ${path.module}/dashboards/network_metrics_dashboard.json ${grafana_folder.Network.uid} ${var.grafana_instance_url} ${var.grafana_service_account_token}"
+    command = "${path.module}/scripts/upload_dashboard.sh ${path.module}/dashboards/network_metrics_dashboard.json ${grafana_folder.network.uid} ${var.grafana_instance_url} ${var.grafana_service_account_token}"
   }
-
   provisioner "local-exec" {
     command = "cat /tmp/grafana_upload_network_metrics_dashboard.log || echo '⚠️ Log not found'"
   }
-
   triggers = {
     always_run = timestamp()
   }
-
-  depends_on = [grafana_folder.Network]
+  depends_on = [grafana_folder.network]
 }
 
 resource "null_resource" "upload_dashboard_proxmox_metrics_dashboard" {
   provisioner "local-exec" {
-    command = "${path.module}/scripts/upload_dashboard.sh ${path.module}/dashboards/proxmox_metrics_dashboard.json ${grafana_folder.Proxmox.uid} ${var.grafana_instance_url} ${var.grafana_service_account_token}"
+    command = "${path.module}/scripts/upload_dashboard.sh ${path.module}/dashboards/proxmox_metrics_dashboard.json ${grafana_folder.proxmox.uid} ${var.grafana_instance_url} ${var.grafana_service_account_token}"
   }
-
   provisioner "local-exec" {
     command = "cat /tmp/grafana_upload_proxmox_metrics_dashboard.log || echo '⚠️ Log not found'"
   }
-
   triggers = {
     always_run = timestamp()
   }
-
-  depends_on = [grafana_folder.Proxmox]
+  depends_on = [grafana_folder.proxmox]
 }
 
 resource "null_resource" "upload_dashboard_pfsense_metrics_dashboard" {
   provisioner "local-exec" {
-    command = "${path.module}/scripts/upload_dashboard.sh ${path.module}/dashboards/pfsense_metrics_dashboard.json ${grafana_folder.PFSense.uid} ${var.grafana_instance_url} ${var.grafana_service_account_token}"
+    command = "${path.module}/scripts/upload_dashboard.sh ${path.module}/dashboards/pfsense_metrics_dashboard.json ${grafana_folder.pfsense.uid} ${var.grafana_instance_url} ${var.grafana_service_account_token}"
   }
-
   provisioner "local-exec" {
     command = "cat /tmp/grafana_upload_pfsense_metrics_dashboard.log || echo '⚠️ Log not found'"
   }
-
   triggers = {
     always_run = timestamp()
   }
-
-  depends_on = [grafana_folder.PFSense]
+  depends_on = [grafana_folder.pfsense]
 }
 
 resource "null_resource" "upload_dashboard_srvr_node_metrics_dashboard" {
   provisioner "local-exec" {
-    command = "${path.module}/scripts/upload_dashboard.sh ${path.module}/dashboards/srvr_node_metrics_dashboard.json ${grafana_folder["Srvr-Node"].uid} ${var.grafana_instance_url} ${var.grafana_service_account_token}"
+    command = "${path.module}/scripts/upload_dashboard.sh ${path.module}/dashboards/srvr_node_metrics_dashboard.json ${grafana_folder.srvr_node.uid} ${var.grafana_instance_url} ${var.grafana_service_account_token}"
   }
-
   provisioner "local-exec" {
     command = "cat /tmp/grafana_upload_srvr_node_metrics_dashboard.log || echo '⚠️ Log not found'"
   }
-
   triggers = {
     always_run = timestamp()
   }
-
-  depends_on = [grafana_folder["Srvr-Node"]]
+  depends_on = [grafana_folder.srvr_node]
 }
 
 resource "null_resource" "upload_dashboard_work_node_metrics_dashboard" {
   provisioner "local-exec" {
-    command = "${path.module}/scripts/upload_dashboard.sh ${path.module}/dashboards/work_node_metrics_dashboard.json ${grafana_folder["Work-Node"].uid} ${var.grafana_instance_url} ${var.grafana_service_account_token}"
+    command = "${path.module}/scripts/upload_dashboard.sh ${path.module}/dashboards/work_node_metrics_dashboard.json ${grafana_folder.work_node.uid} ${var.grafana_instance_url} ${var.grafana_service_account_token}"
   }
-
   provisioner "local-exec" {
     command = "cat /tmp/grafana_upload_work_node_metrics_dashboard.log || echo '⚠️ Log not found'"
   }
-
   triggers = {
     always_run = timestamp()
   }
-
-  depends_on = [grafana_folder["Work-Node"]]
+  depends_on = [grafana_folder.work_node]
 }
 
 resource "null_resource" "upload_dashboard_ctrl_node_metrics_dashboard" {
   provisioner "local-exec" {
-    command = "${path.module}/scripts/upload_dashboard.sh ${path.module}/dashboards/ctrl_node_metrics_dashboard.json ${grafana_folder["Ctrl-Node"].uid} ${var.grafana_instance_url} ${var.grafana_service_account_token}"
+    command = "${path.module}/scripts/upload_dashboard.sh ${path.module}/dashboards/ctrl_node_metrics_dashboard.json ${grafana_folder.ctrl_node.uid} ${var.grafana_instance_url} ${var.grafana_service_account_token}"
   }
-
   provisioner "local-exec" {
     command = "cat /tmp/grafana_upload_ctrl_node_metrics_dashboard.log || echo '⚠️ Log not found'"
   }
-
   triggers = {
     always_run = timestamp()
   }
-
-  depends_on = [grafana_folder["Ctrl-Node"]]
+  depends_on = [grafana_folder.ctrl_node]
 }
 
 resource "null_resource" "upload_dashboard_etcd_node_metrics_dashboard" {
   provisioner "local-exec" {
-    command = "${path.module}/scripts/upload_dashboard.sh ${path.module}/dashboards/etcd_node_metrics_dashboard.json ${grafana_folder["Etcd-Node"].uid} ${var.grafana_instance_url} ${var.grafana_service_account_token}"
+    command = "${path.module}/scripts/upload_dashboard.sh ${path.module}/dashboards/etcd_node_metrics_dashboard.json ${grafana_folder.etcd_node.uid} ${var.grafana_instance_url} ${var.grafana_service_account_token}"
   }
-
   provisioner "local-exec" {
     command = "cat /tmp/grafana_upload_etcd_node_metrics_dashboard.log || echo '⚠️ Log not found'"
   }
-
   triggers = {
     always_run = timestamp()
   }
-
-  depends_on = [grafana_folder["Etcd-Node"]]
+  depends_on = [grafana_folder.etcd_node]
 }
 
 resource "null_resource" "upload_dashboard_ipmi_metrics_dashboard" {
   provisioner "local-exec" {
-    command = "${path.module}/scripts/upload_dashboard.sh ${path.module}/dashboards/ipmi_metrics_dashboard.json ${grafana_folder.Proxmox.uid} ${var.grafana_instance_url} ${var.grafana_service_account_token}"
+    command = "${path.module}/scripts/upload_dashboard.sh ${path.module}/dashboards/ipmi_metrics_dashboard.json ${grafana_folder.proxmox.uid} ${var.grafana_instance_url} ${var.grafana_service_account_token}"
   }
-
   provisioner "local-exec" {
     command = "cat /tmp/grafana_upload_ipmi_metrics_dashboard.log || echo '⚠️ Log not found'"
   }
-
   triggers = {
     always_run = timestamp()
   }
-
-  depends_on = [grafana_folder.Proxmox]
+  depends_on = [grafana_folder.proxmox]
 }
