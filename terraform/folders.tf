40b75d2734d75f9e9355ef6f0f3a598ac05d0c44
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2025-04-18 16:34:36 -0400

    update
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/folders.tf
+++ terraform/folders.tf
@@ -1,87 +1,87 @@
-resource "grafana_folder" "Proxmox" {
-  title    = "Proxmox"
+resource "grafana_folder" "proxmox" {
+  title = "Proxmox"
 }
 
 output "Proxmox_folder_uid" {
-  value = grafana_folder.Proxmox.uid
+  value = grafana_folder.proxmox.uid
 }
 
-resource "grafana_folder" "PFSense" {
-  title    = "PFSense"
+resource "grafana_folder" "pfsense" {
+  title = "PFSense"
 }
 
 output "PFSense_folder_uid" {
-  value = grafana_folder.PFSense.uid
+  value = grafana_folder.pfsense.uid
 }
 
-resource "grafana_folder" "Work-Node" {
-  title    = "Work-Node"
+resource "grafana_folder" "work_node" {
+  title = "Work-Node"
 }
 
 output "Work_Node_folder_uid" {
-  value = grafana_folder.Work-Node.uid
+  value = grafana_folder.work_node.uid
 }
 
-resource "grafana_folder" "Srvr-Node" {
-  title    = "Srvr-Node"
+resource "grafana_folder" "srvr_node" {
+  title = "Srvr-Node"
 }
 
 output "Srvr_Node_folder_uid" {
-  value = grafana_folder.Srvr-Node.uid
+  value = grafana_folder.srvr_node.uid
 }
 
-resource "grafana_folder" "Etcd-Node" {
-  title    = "Etcd-Node"
+resource "grafana_folder" "etcd_node" {
+  title = "Etcd-Node"
 }
 
 output "Etcd_Node_folder_uid" {
-  value = grafana_folder.Etcd-Node.uid
+  value = grafana_folder.etcd_node.uid
 }
 
-resource "grafana_folder" "Ctrl-Node" {
-  title    = "Ctrl-Node"
+resource "grafana_folder" "ctrl_node" {
+  title = "Ctrl-Node"
 }
 
 output "Ctrl_Node_folder_uid" {
-  value = grafana_folder.Ctrl-Node.uid
+  value = grafana_folder.ctrl_node.uid
 }
 
-resource "grafana_folder" "GPU" {
-  title    = "GPU"
+resource "grafana_folder" "gpu" {
+  title = "GPU"
 }
 
 output "GPU_folder_uid" {
-  value = grafana_folder.GPU.uid
+  value = grafana_folder.gpu.uid
 }
 
-resource "grafana_folder" "CPU" {
-  title    = "CPU"
+resource "grafana_folder" "cpu" {
+  title = "CPU"
 }
 
 output "CPU_folder_uid" {
-  value = grafana_folder.CPU.uid
+  value = grafana_folder.cpu.uid
 }
 
-resource "grafana_folder" "Memory" {
-  title    = "Memory"
+resource "grafana_folder" "memory" {
+  title = "Memory"
 }
 
 output "Memory_folder_uid" {
-  value = grafana_folder.Memory.uid
+  value = grafana_folder.memory.uid
 }
 
-resource "grafana_folder" "Disk" {
-  title    = "Disk"
+resource "grafana_folder" "disk" {
+  title = "Disk"
 }
 
 output "Disk_folder_uid" {
-  value = grafana_folder.Disk.uid
+  value = grafana_folder.disk.uid
 }
 
-resource "grafana_folder" "Network" {
-  title    = "Network"
+resource "grafana_folder" "network" {
+  title = "Network"
 }
 
 output "Network_folder_uid" {
-  value = grafana_folder.Network.uid
+  value = grafana_folder.network.uid
 }
