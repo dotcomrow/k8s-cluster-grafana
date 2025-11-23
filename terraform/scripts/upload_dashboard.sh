b96e44d93993cef3cf3ee7edc42448c2d3f053b2
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2025-11-23 17:28:25 -0500

    udpate
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/scripts/upload_dashboard.sh
+++ terraform/scripts/upload_dashboard.sh
@@ -8,7 +8,7 @@ API_TOKEN="$4"
 
 LOG_FILE="/tmp/grafana_upload_$(basename "$DASHBOARD_FILE" .json).log"
 
-echo "API Token used: ${API_TOKEN}" | tee "$LOG_FILE"
+echo "API Token in use: ${API_TOKEN}" | tee "$LOG_FILE"
 
 if [[ ! -f "$DASHBOARD_FILE" ]]; then
   echo "❌ Dashboard file not found : $DASHBOARD_FILE" | tee -a "$LOG_FILE"
